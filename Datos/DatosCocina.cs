49ffa90f7ec317bb1dfd2c4da0f15d959d1daf56
Ronatsu <ronald.alfarohidalgo@ucrso.info>
2018-06-23 14:36:39 -0600

    ESTO SI LO PUEDEN AGREGAR

---
+++ Datos/DatosCocina.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Entidad;
+
+namespace Datos
+{
+    public class DatosCocina
+    {
+        private dbLenguajesProyectoEntities db = new dbLenguajesProyectoEntities();
+        public List<PLATO_ORDEN_Result> platoOrden(int ordenID)
+        {
+            return db.PLATO_ORDEN(ordenID).ToList();
+        }
+        public List<ORDEN_VISTA> orden()
+        {
+            return db.ORDEN_VISTA.ToList();
+        }
+    }
+}
---
+++ Negocio/CocinaNegocio.cs
@@ -0,0 +1,20 @@
+﻿using Datos;
+using Entidad;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Negocio
+{
+    public class CocinaNegocio
+    {
+        private DatosCocina cocina = new DatosCocina();
+        public List<PLATO_ORDEN_Result> platoOrden(int ordenID)
+        {
+            return cocina.platoOrden(ordenID);
+        }
+        public List<ORDEN_VISTA> orden()
+        {
+            return cocina.orden();
+        }
+    }
+}
