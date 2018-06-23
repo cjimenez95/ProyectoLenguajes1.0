fa53f43d2a38456b4f1acbe2b82ff19a2cbb0958
Ronatsu <ronald.alfarohidalgo@ucrso.info>
2018-06-23 15:18:41 -0600

    Cocina echa sin cambio de colores

--- Presentacion/InicioCocina.aspx
+++ Presentacion/InicioCocina.aspx
@@ -4,7 +4,23 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <p>
         <br />
-        <asp:GridView ID="GridCocina" runat="server">
+        <asp:GridView ID="GridCocina" runat="server" AutoGenerateColumns="False" OnRowDataBound="GridCocina_RowDataBound">
+            <Columns>
+                <asp:BoundField DataField="PedidoID" HeaderText="Numero Pedido" />
+                <asp:BoundField DataField="NombreUsusario" HeaderText="Nombre Cliente" />
+                <asp:BoundField DataField="ESTADO" HeaderText="Estado del Pedido" />
+                <asp:TemplateField HeaderText="Platos">
+                    <ItemTemplate>
+                        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                            <Columns>
+                                <asp:BoundField DataField="Nombre" HeaderText="Plato" />
+                                <asp:BoundField DataField="Cantidad" HeaderText="Cantidad" />
+                                <asp:BoundField DataField="Especificacion" HeaderText="Indicacion" />
+                            </Columns>
+                        </asp:GridView>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
         </asp:GridView>
     </p>
     <p>
--- Presentacion/InicioCocina.aspx.cs
+++ Presentacion/InicioCocina.aspx.cs
@@ -1,24 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using Negocio;
 using System.Web.UI.WebControls;
 
 namespace Presentacion
 {
     public partial class InicioCocina : System.Web.UI.Page
     {
+        private CocinaNegocio cocina = new CocinaNegocio();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            cargarOrden();
         }
-
+        private void cargarOrden()
+        {
+            GridCocina.DataSource = cocina.orden();
+            GridCocina.DataBind();
+        }
         private void colorOrden()
         {
             for (int i = 0; i < GridCocina.Rows.Count; i++)
             {
                 String valor = GridCocina.Rows[i].Cells[2].Text;
+            }
+        }
+
+        protected void GridCocina_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            GridView gridPlatos;
+            GridViewRow item;
+            int ordenID;
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                item = e.Row;
+                ordenID = int.Parse(item.Cells[0].Text);
+                gridPlatos = (GridView)e.Row.Cells[3].Controls[1];
+
+                gridPlatos.DataSource = cocina.platoOrden(ordenID);
+                gridPlatos.DataBind();
             }
         }
     }
